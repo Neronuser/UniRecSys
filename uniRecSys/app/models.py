497d04919202ead4d713fcd1d8bf296c19411b22
Neronuser <r.shaptala@gmail.com>
2015-09-18 17:05:33 +0300

    "Added Item API"

--- uniRecSys/app/models.py
+++ uniRecSys/app/models.py
@@ -1,10 +1,15 @@
 from uniRecSys.app import db
+from flask.ext.mongorest.resources import Resource
 
 
 class User(db.Document):
     email = db.EmailField(unique=True, required=True)
-    login = db.StringFiels(unique=True, max_length=20)
+    login = db.StringField(unique=True, max_length=20)
     password = db.StringField(max_length=30)
+
+
+class UserResource(Resource):
+    document = User
 
 
 class Item(db.Document):
@@ -12,8 +17,15 @@ class Item(db.Document):
     description = db.StringField(max_length=200)
 
 
+class ItemResource(Resource):
+    document = Item
+
+
 class Score(db.Document):
     score = db.IntField(min_value=1, max_value=5)
     user = db.ReferenceField(User)
     item = db.ReferenceField(Item)
 
+
+class ScoreResource(Resource):
+    document = Score
--- uniRecSys/app/views.py
+++ uniRecSys/app/views.py
@@ -1,4 +1,14 @@
-from uniRecSys.app import app
+from uniRecSys.app import app, api
+from uniRecSys.app.models import ItemResource
+from flask.ext.mongorest.views import ResourceView
+from flask.ext.mongorest import operators as ops
+from flask.ext.mongorest import methods
+
+
+@api.register(name='items', url='/items/')
+class ItemView(ResourceView):
+    resource = ItemResource
+    methods = [methods.Create, methods.Update, methods.Fetch, methods.List]
 
 
 @app.route("/")
