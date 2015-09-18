c0cd4170b94eb42b0b3d64b5f64abe64dc3f9940
Neronuser <r.shaptala@gmail.com>
2015-09-18 15:52:58 +0300

    "Added score model"

--- uniRecSys/app/models.py
+++ uniRecSys/app/models.py
@@ -2,15 +2,18 @@ from uniRecSys.app import db
 
 
 class User(db.Document):
-    email = db.StringField(required=True)
-    login = db.StringFiels(max_length=20)
+    email = db.EmailField(unique=True, required=True)
+    login = db.StringFiels(unique=True, max_length=20)
     password = db.StringField(max_length=30)
 
 
 class Item(db.Document):
-    name = db.StringField(max_length=20)
+    name = db.StringField(unique=True, max_length=20)
     description = db.StringField(max_length=200)
 
 
-# TODO: class Score
+class Score(db.Document):
+    score = db.IntField(min_value=1, max_value=5)
+    user = db.ReferenceField(User)
+    item = db.ReferenceField(Item)
 
