d3d85aef66f4ae456c8e260ad4268de8896501ca
Neronuser <r.shaptala@gmail.com>
2015-09-24 23:48:12 +0300

    "Added Item API"

--- uniRecSys/app/views.py
+++ uniRecSys/app/views.py
@@ -1,5 +1,5 @@
 from uniRecSys.app import app, api
-from uniRecSys.app.models import ItemResource
+from uniRecSys.app.models import ItemResource, UserResource, ScoreResource
 from flask.ext.mongorest.views import ResourceView
 from flask.ext.mongorest import operators as ops
 from flask.ext.mongorest import methods
@@ -8,6 +8,18 @@ from flask.ext.mongorest import methods
 @api.register(name='items', url='/items/')
 class ItemView(ResourceView):
     resource = ItemResource
+    methods = [methods.Create, methods.Update, methods.Fetch, methods.List]
+
+
+@api.register(name='users', url='/users/')
+class UserView(ResourceView):
+    resource = UserResource
+    methods = [methods.Create, methods.Update, methods.Fetch, methods.List]
+
+
+@api.register(name='scores', url='/scores/')
+class ScoreView(ResourceView):
+    resource = ScoreResource
     methods = [methods.Create, methods.Update, methods.Fetch, methods.List]
 
 
